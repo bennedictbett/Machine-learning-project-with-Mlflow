b51ff322947244b5214dea32bd3f9f8cd7042eaa
bennedictbett <benedictbett08@gmail.com>
2025-12-23 20:06:41 +0300

    added data ingestion pipeline

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Machine-learning-project-with-Mlflow
+
+## Workflow 
+
+1. Update config.ymal
+2. Update Schema.ymal 
+3. Update parems.ymal
+4. Update the entities 
+5. Update the config manager in src config 
+6. Update the components 
+7. Update the pipeline
+8. Upadate main.py
+9. Update app.py 
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 logger.info("Setup script is being executed.")
