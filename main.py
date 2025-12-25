19a6f966e3125ce89158988336b84c128b3dc37d
bennedictbett <benedictbett08@gmail.com>
2025-12-25 22:40:41 +0300

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -13,3 +14,14 @@ except Exception as e:
         raise e
 
 logger.info("Setup script is being executed.")
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,6 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import DataIngestionConfig  
+from mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig) 
 
 
 class ConfigurationManager:
@@ -29,3 +29,20 @@ class ConfigurationManager:
         )
         
         return data_ingestion_config
+    
+
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation 
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=Path(config.root_dir),
+            STATUS_FILE=Path(config.STATUS_FILE),  # Changed back to uppercase
+            unzip_data_dir=Path(config.unzip_data_dir),
+            all_schema=schema
+    )
+
+        return data_validation_config
